3a8bde5cdd25077f48b931ec2bf44513ae69b21c
Squid1917 <82701629+Squid1917@users.noreply.github.com>
2023-09-19 18:44:30 +0100

    Delete Python/PlaylistOrder.py

--- Python/PlaylistOrder.py
+++
@@ -1,7 +0,0 @@
-import spotipy
-from spotipy import oauth2
-
-PORT_NUMBER = 8080
-SPOTIPY_CLIENT_ID = 'your_client_id'
-SPOTIPY_CLIENT_SECRET = 'your_client_secret'
-SPOTIPY_REDIRECT_URI = 'http://localhost:8080'
