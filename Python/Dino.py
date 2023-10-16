02a64bb1ffc76e41d75733a96aaa08764d0806a8
Squid1917 <acanterdale@gmail.com>
2023-10-16 13:20:14 +0100

    Create Dino.py

---
+++ Python/Dino.py
@@ -0,0 +1,29 @@
+def CheckInput(Message):
+    Choice = input(Message).upper()
+    if Choice == "Y":
+        return True
+    if Choice == "N":
+        return False
+    else:
+        print("Please try again")
+        CheckInput(Message)
+
+Dino = ["Triceratops","Stegosaurus","Trex","Nigersaurous","Pterodactyl","Diplodocus"]
+print("Select: Triceratops,Stegosaurus,Trex,Nigersaurous,Pterodactyl,Diplodocus")
+if CheckInput("Has Three Horns Y/N: "):
+    print(f"Your Dino is {Dino[0]}")
+else:
+    if CheckInput("Has Spikes On Back Y/N: "):
+        print(f"Your Dino is {Dino[1]}")
+    else:
+        if CheckInput("Has Tiny Hands Y/N: "):
+            print(f"Your Dino is {Dino[2]}")
+        else:
+            if CheckInput("Looks Like Lewis Y/N: "):
+                print(f"Your Dino is {Dino[3]}")
+            else:
+                if CheckInput("Can It Fly Y/N: "):
+                    print(f"Your Dino is {Dino[4]}")
+                else:
+                    if CheckInput("Is It an Ali-A meme Y/N: "):
+                        print(f"Your Dino is {Dino[5]}")
