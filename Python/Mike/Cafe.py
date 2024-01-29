73c25d9d9a4f9241f993f51dae61c96b8dacba13
Squid1917 <acanterdale@gmail.com>
2024-01-29 14:25:15 +0000

    Update Cafe.py

--- Python/Mike/Cafe.py
+++ Python/Mike/Cafe.py
@@ -1,22 +1,19 @@
-Items = ["Coffee","Tea","Muffin","Smoothie","Toast"]
-Prices = {"Coffee": 2.50, "Tea": 1.50, "Muffin" : 2.00, "Smoothie": 3.00, "Toast": 5}
-CurrentItems = []
-TotalPrice = 0.00
+Items = ["Coffee","Tea","Muffin","Smoothie","Toast"] #? Define Items
+Prices = {"Coffee": 2.50, "Tea": 1.50, "Muffin" : 2.00, "Smoothie": 3.00, "Toast": 5} #? Define the item prices in a dict
+CurrentItems = []  #? Define basket
+TotalPrice = 0.00  #? Define total price
 
-print("Welcome to the cafe we have " , Items)
-
+print("Welcome to the cafe we have " , Items)  #? Print out all the items
 print("Type 'Exit' after last item")
-while True:
-    Item = input("Enter item to add: ")
-    if Item in Items:
-        CurrentItems.append(Item)
-    elif Item == "Exit":
-        break 
+
+while True: #? Loop forever
+    Item = input("Enter item to add: ") #? Input the item
+    if Item in Items: #? Check if valid
+        CurrentItems.append(Item) #? Add item to basket
+    elif Item == "Exit":  #? Exit if last item
+        break  
     else:
-        print("Error: Item not in database")
+        print("Error: Item not in database") #? Error Handleing
 
-for Item in CurrentItems: 
-    TotalPrice += Prices[Item]
-    print(f"{Item} is {Prices[Item]}")
-
-print(f"Total: {TotalPrice}")
+for Item in CurrentItems:  #? Loop over basket
+    TotalPrice += Prices[Item] #? Total the final price
